f56946bc6c48a2dfc8cb1cfe766e94d79a85c77e
“santhosh-sethu-99” <sethuramansanthoshkumar@gmail.com>
2021-05-05 18:01:15 +0530

    generic

---
+++ lib/constants.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+// Greetings
+const kHello = Text(
+  'Hellö,',
+  style: TextStyle(
+    fontSize: 80,
+    fontFamily: 'Itim',
+    color: Colors.white60,
+  ),
+);
+
+// About
+const kIAm = Text(
+  ' I\'m ',
+  style: TextStyle(
+    fontSize: 80,
+    fontFamily: 'Itim',
+    color: Colors.white60,
+  ),
+);
+
+const kFirstName = Text(
+  'Santhosh Kumar',
+  style: TextStyle(
+    fontSize: 80,
+    fontFamily: 'Itim',
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  ),
+);
+
+const kLastName = Text(
+  '        Sethuraman',
+  style: TextStyle(
+    fontSize: 80,
+    fontFamily: 'Itim',
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  ),
+);
+
+const kAboutYou = '';
+
+// Technical Skills
+const kTechnicalSkills = [
+  'Programming in C',
+  'Flutter',
+  'Dart',
+  'Java',
+];
+
+// Soft Skills
+const kSoftSkills = [
+  'Self-Learning',
+  'Constant Learning',
+  'Logical Thinking',
+  'Open Mindedness',
+  'Adaptability',
+  'Empathy',
+  'Confidence',
+  'Motivating',
+];
+
+// Experience
+const kExperience = {
+  'companyName': 'ChennaiHub',
+  'fromDate': 'December, 2020',
+  'toDate': 'March, 2021',
+  'role': 'Software Developer (Mobile Developement)',
+  'emplomentType': 'Intern',
+  'description':
+      'This internship gained me some real world software engineering experience. I was assigned to work as a Mobile Developer. I was able to improve my code quality and logical abilities in this four month period. Worked closely with other team members to develop an mobile app for RealEstate Platform.'
+};
+
+// Links
+const kGithub = 'https://github.com/santhosh-sethu-99';
+
+const kLinkedin =
+    'https://www.linkedin.com/in/santhosh-kumar-sethuraman-0b6076203';
+
+const kPhoneNumber = '+91-9047097827';
+
+const kEmailId = 'sethuramansanthoshkumar@gmail.com';
+
+// Quotes
+const kCodingQuote = 'Talk is Cheap, Show me the Code!';
