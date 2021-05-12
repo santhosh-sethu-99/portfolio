607db2ba089577aa905c15b0f9e593685ad39add
santhosh-sethu-99 <76466079+santhosh-sethu-99@users.noreply.github.com>
2021-05-12 18:54:31 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class Portfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Santhosh Kumar Sethuraman",
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
